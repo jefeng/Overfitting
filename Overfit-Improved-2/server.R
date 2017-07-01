68c746816a9213b1c3e7619574626e929125b6ce
Jinglin Feng <jennyklosever@gmail.com>
2017-07-01 14:30:46 -0400

    Add files via upload

--- Overfit-Improved-2/server.R
+++ Overfit-Improved-2/server.R
@@ -1,154 +1,147 @@
 library(shiny)
 library(shinythemes)
-function(input, output) {
+shinyServer(function(input, output,session) {
   
-  
-  test <- eventReactive(input$goButton, {
-    
-    
-    
-    n=input$n
-    
-    p=input$p
-    k=input$k
-    
-    
-    xmat=matrix(0,n,k)
-    
-    y1=rnorm(n,0,1)
-    
-    if(p!=0){
-      x1=rnorm(n,y1,1/abs(p))
-      x2=rnorm(n,y1,1/abs(p))
-    }else{
+    plotdata<-reactive({
+      n=input$n
+      p=input$p
+      k=input$k
+      xmat=matrix(0,n,k)
       
-      x1=rnorm(n,0,1 )
-      x2=rnorm(n,0,1 )
-      
-    }
-    
-    
-    if(p!=0){
-      for(i in 1:k){
-        
-        xmat[,i]=rnorm(n,y1,1/abs(p))
-        
-        
-      } }else{
-        for(i in 1:k){
-          
-          xmat[,i]=rnorm(n,0,1 )
-          
-          
+      if(input$plot>0){
+        y1<-rnorm(n,0,1)
+        if(p!=0){
+          x1=rnorm(n,y1,1/abs(p))
+          x2=rnorm(n,y1,1/abs(p))
+        }else{
+          x1=rnorm(n,0,1 )
+          x2=rnorm(n,0,1 )
         }
-      }
-    
-    
-    R2=0
-    
-    for(i in 1:k){
-      
-      R2[i]=cor( y1,xmat[,i])
-      
-    }
-    
-    kk=which.max(abs(R2))
-    
-    y=rnorm(n,0,1)
-    xmat=matrix(0,n,k)
-    mm<-lm(y~xmat[,kk])
-    
-    mm2<-lm(y1~xmat[, k-kk+1])
-    
-    # plot(density(y-mm$fitted.values),ylim=c(0,0.8))
-    
-    d1<-density(y1-mm2$fitted.values)
-    
-    
-    
-  })
-  
-  
-  output$plot1 <- renderPlot({
-    
-    
-    n=input$n
-    
-    p=input$p
-    k=input$k
-    
-    
-    xmat=matrix(0,n,k)
-    
-    
-    
-    y2=rnorm(n,0,1)
-    
-    
-    if(p!=0){
-      x1=rnorm(n,y2,1/abs(p))
-      x2=rnorm(n,y2,1/abs(p))
-    }else{
-      
-      x1=rnorm(n,0,1 )
-      x2=rnorm(n,0,1 )
-      
-    }
-    
-    
-    
-    
-    if(p!=0){
-      for(i in 1:k){
-        
-        xmat[,i]=rnorm(n,y2,1/abs(p))
-        
-        
-      } }else{
-        for(i in 1:k){
-          
-          xmat[,i]=rnorm(n,0,1 )
-          
-          
+        if(p!=0){
+          for(i in 1:k)
+            xmat[,i]=rnorm(n,y1,1/abs(p))
         }
+        else{
+          for(i in 1:k)
+            xmat[,i]=rnorm(n,0,1)
+        }
+        
+        R2=0
+        for(i in 1:k)
+          R2[i]=cor( y1,xmat[,i])
+        
+        data<-list(R2,y1,xmat)
       }
+        data
+    })
     
+    bluey<-reactive({
+      n=input$n
+      if(input$validate>0)
+        y<-rnorm(n,0,1)
+      y
+    })
     
-    
-    
-    R2=0
-    
-    for(i in 1:k){
+    plot2<-renderPlot({
+      n=input$n
+      k=input$k
+      mydata<-plotdata()
+      R2<-unlist(mydata[1])
+      y2<-unlist(mydata[2])
+      xmat<-array(unlist(mydata[3]),dim=c(n,k))
+      kk=which.max(abs(R2)) #best
       
-      R2[i]=cor( y2,xmat[,i])
-      
-    }
-    
-    kk=which.max(abs(R2))
-    
-    mm<-lm(y2~xmat[,kk])
-    
-    y2-mm$fitted.values
-   
-    
-    # hist(y-mm$fitted.values,freq = F)
-    
-    d2<-density(y2-mm$fitted.values)
-    
-    plot(range(d1$x, d2$x), range(d1$y, d2$y), type = "n", xlab = "Residual",
-         ylab = "Density", main="")
-    
-    if (input$goButton) {
-      lines(d1,col="blue",lwd=2)
+      mm<-lm(y2~xmat[,kk]) # Best Chosen X
+
+      d2<-density(y2-mm$fitted.values,bw = 0.55) # Pick the best X
+      plot(range(d2$x), range(d2$y), type = "n", xlab = "Residual",
+           ylab = "Density", main="")
       lines(d2, col="black",lwd=2)
-   
-    } 
-  })
-  
-  output$text <- renderText({ 
-    HTML(paste("Black line: Original y subtracts the best X",
-               "Blue line: New generated y subtracts a randomly chosen X", sep ="<br/>"))
-  })
-  
-  
-}
+    })
+
+    plot1<-renderPlot({
+      n=input$n
+      k=input$k
+      mydata<-plotdata()
+      R2<-unlist(mydata[1])
+      y2<-unlist(mydata[2])
+      xmat<-array(unlist(mydata[3]),dim=c(n,k))
+      kk=which.max(abs(R2))
+      mm<-lm(y2~xmat[,kk]) # Best Chosen X
+      
+      d2<-density(y2-mm$fitted.values, bw = 0.55) # Pick the best X
+      
+      y1blue<-bluey()
+      kkblue=which.max(abs(R2))
+      xmatblue=array(unlist(mydata[3]),dim=c(n,k)) 
+      mm2blue<-lm(y1blue~xmatblue[, k-kkblue+1]) # Randomly Chosen X
+      d1<-density(y1blue-mm2blue$fitted.values, bw=0.5) # Randomly Chosen X)
+      plot(range(d1$x,d2$x), range(d1$y,d2$y), type = "n", xlab = "Residual",
+           ylab = "Density", main="")
+      lines(d2, col="black",lwd=2)
+      lines(d1, col="blue",lwd=2)
+    })
+    
+    scatterplot<-renderPlot({
+      n=input$n
+      k=input$k
+      mydata<-plotdata()
+      R2<-unlist(mydata[1])
+      y2<-unlist(mydata[2])
+      xmat<-array(unlist(mydata[3]),dim=c(n,k))
+      
+      kk=which.max(abs(R2))
+      mm<-lm(y2~xmat[,kk]) # Best Chosen X
+      plot(xmat[,kk], y2, xlab="Best Chosen X", ylab="Y")
+      abline(mm,col="blue")
+    })
+    
+    
+    observeEvent(input$plot,output$plot<-plot2)
+    observeEvent(input$plot,output$scatter<-scatterplot)
+    observeEvent(input$plot,output$choose<-value1)
+
+    observeEvent(input$validate,output$plot<-plot1)
+    observeEvent(input$validate,output$choose<-value2)
+    
+    value11<-reactive({
+      n=input$n
+      k=input$k
+      mydata<-plotdata()
+      R2<-unlist(mydata[1])
+      y2<-unlist(mydata[2])
+      xmat<-array(unlist(mydata[3]),dim=c(n,k))
+      kk=which.max(abs(R2)) #best
+      best<-cor(y2,xmat[, kk])
+      xx=as.data.frame(best)
+      colnames(xx)=c("Sample Best Correlation")
+      xx
+    })
+    value1<-renderTable({
+      value11()},
+      align="c"
+    )
+    value22<-reactive({
+      n=input$n
+      k=input$k
+      best<-value11()
+      mydata<-plotdata()
+      R2<-unlist(mydata[1])
+      kkblue<-which.max(abs(R2))
+      xmatblue=array(unlist(mydata[3]),dim=c(n,k))
+      y1blue<-bluey()
+      random<-cor(y1blue,xmatblue[, k-kkblue+1])
+      xx=cbind(best,random)
+      xx=as.data.frame(xx)
+      colnames(xx)=c("Sample Best Correlation","Sample Randomly Chosen Correlation")
+      xx
+    })
+    value2<-renderTable({
+      value22()},
+      align="c"
+    )
+    output$text <- renderText({ 
+      HTML(paste("Black line: Original y subtracts estimated y with the best X",
+                 "Blue line: New generated y subtracts estimated y with a randomly chosen X", sep ="<br/>"))
+    })
+})
--- Overfit-Improved-2/ui.R
+++ Overfit-Improved-2/ui.R
@@ -3,20 +3,44 @@ library(shinythemes)
 fluidPage(
   theme = shinytheme("flatly"),
   titlePanel("Overfitting"),
+  tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #1C2C5B}")),
+  tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: #1C2C5B}")),
+  tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: #1C2C5B}")),
   fluidRow(
     column(3, wellPanel(
-      sliderInput("n", "Number of Samples:", min = 5, max = 1000, value = 5 ,
-                  step = 10),
+      sliderInput("n", "Sample Size:", min = 1, max = 1000, value = 5 ,
+                  step = 1),
       sliderInput("p", "True Population Correlation:", min = -1, max = 1 , value = 0,
                   step = 0.01),
       sliderInput("k", "The Number of Variables:", min = 1, max = 100 , value = 3 ,
                   step = 1),
-      actionButton("goButton", "Click here to Validate")
-    )),
-    column(6,offset=1, align="center", 
-           plotOutput("plot1", width = 640, height = 480) 
-    ),
-    div(style = "position:absolute;bottom: 4em;left:42em;",
-        htmlOutput("text"))
+      actionButton("plot", "Click here first to Plot"),
+      br(),
+      br(),
+      
+      actionButton("validate", "Click here later to Validate"))),
+    
+      
+    absolutePanel(
+      id = "controls", class = "myClass", fixed = FALSE,
+      draggable = FALSE, top = 40, left = "auto", right = 510, bottom = "auto",
+      width = 520, height = "auto",
+      plotOutput("plot")),
+    
+      absolutePanel(
+      id = "controls", class = "myClass", fixed = FALSE,
+      draggable = FALSE, top = 40, left = "auto", right = -3, bottom = "auto",
+      width = 500, height = "auto",
+      plotOutput("scatter")),
+    
+      absolutePanel(
+      id = "controls", class = "myClass", fixed = FALSE,
+      draggable = FALSE, top = 460, left = "auto", right = 488, bottom = "auto",
+      width = 500, height = "auto",
+      tableOutput("choose")),
+    
+      conditionalPanel("input.validate != 0",
+      div(style = "position:absolute;bottom: 4em;left:26em;",
+        htmlOutput("text")))
   )
 )
